176018d4ecc6d60547504b01590969133faffa37
Konstantin Grinev <konstantingrinev7@gmail.com>
2022-04-04 12:28:15 +0300

    make functionality to form

--- script.js
+++ script.js
@@ -1,48 +1,66 @@
-"use strict";
+'use strict';
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector(".form");
-const containerWorkouts = document.querySelector(".workouts");
-const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
-const inputDuration = document.querySelector(".form__input--duration");
-const inputCadence = document.querySelector(".form__input--cadence");
-const inputElevation = document.querySelector(".form__input--elevation");
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (postion) {
-        let { latitude } = postion.coords;
-        let { longitude } = postion.coords;
-        let coords = [latitude, longitude];
-  
-        let map = L.map("map").setView(coords, 13);
-  
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
-  
-        
-        map.on('click', function (mapEvent) {
-          let {lat, lng} = mapEvent.latlng;
-          L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup(L.popup({
-              maxWidth: 250, 
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup'}))
-            .setPopupContent('Workout')
-            .openPopup();
+  navigator.geolocation.getCurrentPosition(
+    function (postion) {
+      let { latitude } = postion.coords;
+      let { longitude } = postion.coords;
+      let coords = [latitude, longitude];
 
-          })
-      },
-      function () {
-        alert("Could not get your postion.");
-      }
-    );
-  }
+      map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert('Could not get your postion.');
+    }
+  );
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Display marker
+  let { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});

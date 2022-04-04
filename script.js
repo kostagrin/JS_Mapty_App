f3401d783977261f8549656ed1ad1fd945cb74e0
Konstantin Grinev <konstantingrinev7@gmail.com>
2022-04-04 11:34:50 +0300

    apply "getCurrentPosition"

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (postion) {
+        let { latitude } = postion.coords;
+        let { longitude } = postion.coords;
+        let coords = [latitude, longitude];
+  
+        let map = L.map("map").setView(coords, 13);
+  
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+  
+        
+        map.on('click', function (mapEvent) {
+          let {lat, lng} = mapEvent.latlng;
+          L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(L.popup({
+              maxWidth: 250, 
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup'}))
+            .setPopupContent('Workout')
+            .openPopup();
+
+          })
+      },
+      function () {
+        alert("Could not get your postion.");
+      }
+    );
+  }
